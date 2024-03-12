66aa584e2db82e8b1f4277d8e9e405289c956d5f
Anurag Kumar <anuragabcr@gmail.com>
2024-03-12 17:57:39 +0000

    SG update

--- day-2/c1-version.tf
+++ day-2/c1-version.tf
@@ -26,6 +26,22 @@ resource "aws_security_group" "vpc-web" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # Resource: EC2 Instance
