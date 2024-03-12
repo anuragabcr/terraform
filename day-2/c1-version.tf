7788af2ad1265d03ac4945b53cd9e0aa108b718b
Anurag Kumar <anuragabcr@gmail.com>
2024-03-12 11:30:47 +0000

    update inbound rule

--- day-2/c1-version.tf
+++ day-2/c1-version.tf
@@ -14,3 +14,31 @@ provider "aws" {
   region = "ap-south-1"
   profile = "default"
 }
+
+# Resource: Security Group
+resource "aws_security_group" "http_sg" {
+  name        = "http_security_group"
+  description = "Allow HTTP traffic"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from anywhere
+  }
+
+  tags = {
+    Name = "HTTP Security Group"
+  }
+}
+
+# Resource: EC2 Instance
+resource "aws_instance" "day2_ec2" {
+  ami = "ami-0ba259e664698cbfc"
+  instance_type = "t2.micro"
+  user_data = file("${path.module}/simpleWeb.sh")
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+  security_groups = [aws_security_group.http_sg.name] # Attach the security group to the instance
+}
