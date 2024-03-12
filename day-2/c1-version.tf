eac7be333d36dca9e3b6ba3064fc74d8ddcedcc0
Anurag Kumar <anuragabcr@gmail.com>
2024-03-12 17:51:12 +0000

    SSH removed from SG

--- day-2/c1-version.tf
+++ day-2/c1-version.tf
@@ -15,26 +15,6 @@ provider "aws" {
   profile = "default"
 }
 
-# Create Security Group - SSH Traffic
-resource "aws_security_group" "vpc-ssh" {
-  name        = "vpc-ssh"
-  description = "Dev VPC SSH"
-  ingress {
-    description = "Allow Port 22"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    description = "Allow all ip and ports outboun"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # Create Security Group - Web Traffic
 resource "aws_security_group" "vpc-web" {
   name        = "vpc-web"
@@ -44,22 +24,6 @@ resource "aws_security_group" "vpc-web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow Port 443"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all ip and ports outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
