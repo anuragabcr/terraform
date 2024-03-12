844d7fd008e2dfd45ec2927f0e4f5201915acd91
Anurag Kumar <anuragabcr@gmail.com>
2024-03-12 17:24:07 +0000

    bash script update

--- day-2/simpleWeb.sh
+++ day-2/simpleWeb.sh
@@ -1,15 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 # Instance Identity Metadata Reference - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl enable httpd
-sudo service httpd start  
-sudo echo '<h1>Welcome to StackSimplify - APP-1</h1>' | sudo tee /var/www/html/index.html
+sudo apt update -y
+sudo apt install -y apache2
+sudo systemctl enable apache2
+sudo systemctl start apache2  
+sudo bash -c 'echo "<h1>Welcome to Terraform - APP-1</h1>" > /var/www/html/index.html'
 sudo mkdir /var/www/html/app1
-sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
-#sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-sudo curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
-# AWS Documentation to retrieve EC2 Instance Data
-# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
-
+sudo bash -c 'echo "<!DOCTYPE html> <html> <body style=\"background-color:rgb(250, 210, 210);\"> <h1>Welcome to Terraform - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>" > /var/www/html/app1/index.html'
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+sudo bash -c "curl -H \"X-aws-ec2-metadata-token: $TOKEN\" http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html"
--- day-3/simpleWeb.sh
+++ day-3/simpleWeb.sh
@@ -4,8 +4,8 @@ sudo apt update -y
 sudo apt install -y apache2
 sudo systemctl enable apache2
 sudo systemctl start apache2  
-sudo bash -c 'echo "<h1>Welcome to StackSimplify - APP-1</h1>" > /var/www/html/index.html'
+sudo bash -c 'echo "<h1>Welcome to Terraform - APP-1</h1>" > /var/www/html/index.html'
 sudo mkdir /var/www/html/app1
-sudo bash -c 'echo "<!DOCTYPE html> <html> <body style=\"background-color:rgb(250, 210, 210);\"> <h1>Welcome to Stack Simplify - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>" > /var/www/html/app1/index.html'
+sudo bash -c 'echo "<!DOCTYPE html> <html> <body style=\"background-color:rgb(250, 210, 210);\"> <h1>Welcome to Terraform - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>" > /var/www/html/app1/index.html'
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 sudo bash -c "curl -H \"X-aws-ec2-metadata-token: $TOKEN\" http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html"
