e6aa2a85686f8f7e17deec1d7d37874cd48b9029
Anurag Kumar <anuragabcr@gmail.com>
2024-03-13 07:21:00 +0000

    output ip reference fix

--- day-4/6-output.tf
+++ day-4/6-output.tf
@@ -1,23 +1,23 @@
 # Output - FOR loop with list of IPs
 output "IPs_list" {
   description = "List of IP address"
-  value = [for instance in aws_instance.day4_ec2: instance.public_IP]
+  value = [for instance in aws_instance.day4_ec2: instance.public_ip]
 }
 
 # Output - MAP of IPs
 output "IPs_map" {
   description = "MAP of IP address"
-  value = {for instance in aws_instance.day4_ec2: instance.name=> instance.public_IP}
+  value = {for instance in aws_instance.day4_ec2: instance.tags.Name=> instance.public_ip}
 }
 
 # Output - MAP of IPs
 output "IPs_map2" {
   description = "MAP of IP address"
-  value = {for index, instance in aws_instance.day4_ec2: index=>instance.public_IP}
+  value = {for index, instance in aws_instance.day4_ec2: index=>instance.public_ip}
 }
 
 # Output - Splat operator
 output "IPs_splat" {
   description = "IP address using splat operator"
-  value = aws_instance.day4_ec2[*].public_IP
+  value = aws_instance.day4_ec2[*].public_ip
 }
