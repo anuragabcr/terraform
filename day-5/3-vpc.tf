fc0b06d77d0935cc21e8b7cc082dba371b1aa2b4
Anurag Kumar <anuragabcr@gmail.com>
2024-03-13 18:43:29 +0000

    CIDR block update

--- day-5/3-vpc.tf
+++ day-5/3-vpc.tf
@@ -7,8 +7,8 @@ module "vpc" {
   name = "vpc-dev"
   cidr = "10.10.0.0/16"
   azs = ["ap-south-1a", "ap-south-1b"]
-  public_subnets = ["10.10.1.0/16", "10.10.2.0/16"]
-  private_subnets = ["10.10.3.0/16", "10.10.4.0/16"]
+  public_subnets = ["10.10.1.0/24", "10.10.2.0/24"]
+  private_subnets = ["10.10.3.0/24", "10.10.4.0/24"]
 
   # Database Subnets
   create_database_subnet_group = true
