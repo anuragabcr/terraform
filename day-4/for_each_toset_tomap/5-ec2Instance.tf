07ca3aceea4289a27886a27191930e38de8a5a7f
Anurag Kumar <anuragabcr@gmail.com>
2024-03-13 07:52:19 +0000

    list to set

--- day-4/for_each_toset_tomap/5-ec2Instance.tf
+++ day-4/for_each_toset_tomap/5-ec2Instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "day4_ec2" {
   # instance_type = var.instance_type_list[0]
   instance_type = var.instance_type_map["dev"]
   user_data = file("${path.module}/simpleWeb.sh")
-  for_each = data.aws_availability_zones.zones.names
+  for_each = toset(data.aws_availability_zones.zones.names)
   availability_zone = each.key
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id ]
   tags = {
