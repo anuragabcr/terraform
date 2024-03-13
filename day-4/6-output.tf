b68e454a24a72981484192e23c843dd9dc3afe8a
Anurag Kumar <anuragabcr@gmail.com>
2024-03-13 07:15:58 +0000

    instance name fix

--- day-4/6-output.tf
+++ day-4/6-output.tf
@@ -7,13 +7,13 @@ output "IPs_list" {
 # Output - MAP of IPs
 output "IPs_map" {
   description = "MAP of IP address"
-  value = {for instance in aws_instance.aws_instance.day4_ec2: instance.name=> instance.public_IP}
+  value = {for instance in aws_instance.day4_ec2: instance.name=> instance.public_IP}
 }
 
 # Output - MAP of IPs
 output "IPs_map2" {
   description = "MAP of IP address"
-  value = {for index, instance in aws_aws_instance.day4_ec2: index=>instance.public_IP}
+  value = {for index, instance in aws_instance.day4_ec2: index=>instance.public_IP}
 }
 
 # Output - Splat operator
