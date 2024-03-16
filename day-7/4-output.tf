8554c367fd86a9e8423ee96863aeae1970f7fe96
Anurag Kumar <anuragabcr@gmail.com>
2024-03-16 22:06:40 +0000

    website url

--- day-7/4-output.tf
+++ day-7/4-output.tf
@@ -1,4 +1,4 @@
 # Website URL
 output "website_url" {
-  value = aws_s3_bucket.terraform-demo-43234.website[0]
+  value = "http://${aws_s3_bucket.terraform-demo-43234.bucket}.s3-website.${aws_s3_bucket.terraform-demo-43234.region}.amazonaws.com"
 }
