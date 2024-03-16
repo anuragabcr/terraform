a133f3d63d14d906d8efd586b632b92e7a89a13b
Anurag Kumar <anuragabcr@gmail.com>
2024-03-16 21:14:34 +0000

    testing 3

--- day-7/3-policy.tf
+++ day-7/3-policy.tf
@@ -10,13 +10,15 @@ resource "aws_s3_bucket_policy" "open_access" {
 
   policy = jsonencode({
     Version = "2012-10-17"
+    Id      = "Public_access"
     Statement = [
       {
+        Sid = "IPAllow"
         Effect = "Allow"
         Principal = "*"
-        Action = "s3:GetObject"
+        Action = ["s3:GetObject"]
         Resource = "${aws_s3_bucket.terraform-demo-43234.arn}/*"
-      }
+      },
     ]
   })
 }
