337745d09708ce07c80f8276845a2ba18cb727f9
Anurag Kumar <anuragabcr@gmail.com>
2024-03-16 17:41:12 +0000

    policy fix

--- day-6/4-attach_policy.tf
+++ day-6/4-attach_policy.tf
@@ -1,6 +1,6 @@
 # Attach IAM policy with user
 resource "aws_iam_policy_attachment" "S3_user" {
     name = "S3_ReadOnly"
-  users = aws_iam_user.new-user
-  policy_arn = aws_iam_policy.s3_read
+  users = [aws_iam_user.new-user.name]
+  policy_arn = aws_iam_policy.s3_read.arn
 }
